f8dd45ce0c607c5f1b333143b8a9aa480711d7fb
likianta <sheerish@qq.com>
2020-11-29 18:58:49 +0800

    Add layout_engine.py

---
+++ lk_qtquick_scaffold/layout_engine.py
@@ -0,0 +1,41 @@
+from .pycomm import adapt_type, PyHandler, QObjectDelegator as Delegator
+
+
+class PyLayoutHelper(PyHandler):
+    
+    def __init__(self):
+        super().__init__(object_name='PyLayoutHelper')
+        #                            ^--------------^
+        #   This name will be available in QML global namespace.
+        
+        self.register_pyfunc(self.align_children)
+    
+    @adapt_type
+    def align_children(self, parent: Delegator, alignment: str):
+        """ Align children to parent.horizontalCenter or verticalCenter.
+        
+        Args:
+            parent
+            alignment ('hcenter'|'vcenter'|native):
+                native: 'horizontalCenter'|'verticalCenter'|'left'|'right'|
+                        'top'|'bottom'
+        
+        Examples:
+            // view.qml
+            Item {
+                Text { anchors.top: parent.top }
+                Rectangle { anchors.bottom: parent.bottom }
+                Component.onCompleted: {
+                    PyLayoutHelper.align_children(this, 'hcenter')
+                }
+            }
+        
+        """
+        if alignment == 'hcenter':
+            alignment = 'horizontalCenter'
+        elif alignment == 'vcenter':
+            alignment = 'verticalCenter'
+        
+        for i in parent.children():
+            i.anchors[alignment] = parent[alignment]
+            #   E.g. i.anchors.horizontalCenter = parent.horizontalCenter
