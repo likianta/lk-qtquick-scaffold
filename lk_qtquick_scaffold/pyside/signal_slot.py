a4064a3314504608856bca83eb02903c3326c903
likianta <likianta@foxmail.com>
2022-04-09 12:03:29 +0800

    Auto convert argtypes

--- lk_qtquick_scaffold/pyside/signal_slot.py
+++ lk_qtquick_scaffold/pyside/signal_slot.py
@@ -5,21 +5,52 @@ from typing import Union
 
 from PySide6.QtCore import Signal
 from PySide6.QtCore import Slot
+from PySide6.QtQml import QJSValue
 
 
-def slot(*args: type, result: Union[str, type, None] = None):
+def slot(*argtypes: type, result: Union[str, type, None] = None):
     if result is not None and result is not str and type(result) is not str:
         result = 'QVariant'
     
+    new_argtypes = []
+    for t in argtypes:
+        if t not in (bool, float, int, str):
+            t = QJSValue
+        new_argtypes.append(t)
+    argtypes = tuple(new_argtypes)
+    del new_argtypes
+    
+    def _auto_convert_argtypes(*args):
+        nonlocal argtypes
+        
+        if len(args) > len(argtypes):
+            self, args = args[0], args[1:]
+        else:
+            self, args = None, args
+            
+        new_args = []
+        
+        for i, a, t in zip(range(len(args)), args, argtypes):
+            if t is QJSValue:
+                new_args.append(a.toVariant())
+            else:
+                new_args.append(a)
+        
+        if self is None:
+            return new_args
+        else:
+            return self, *new_args
+    
     def wrapper(func):
-        return Slot(*args, result=result)(func)
+        delegate_func = lambda *args: func(*_auto_convert_argtypes(*args))
+        return Slot(*argtypes, name=func.__name__, result=result)(delegate_func)
     
     return wrapper
 
 
 class SignalTypeHint:
     
-    def __call__(self, *args: type):
+    def __call__(self, *argtypes: type):
         pass
     
     def connect(self, func):
