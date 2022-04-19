862a2749e8c00eaaaa4d194af011b06d85c8b8de
likianta <likianta@foxmail.com>
2022-04-19 19:52:36 +0800

    Refactor `Application`

--- lk_qtquick_scaffold/pyside/application.py
+++ lk_qtquick_scaffold/pyside/application.py
@@ -1,11 +1,7 @@
-import os.path as xpath
-
 from PySide6.QtCore import QObject
 from PySide6.QtQml import QQmlApplicationEngine
 from PySide6.QtQml import QQmlContext
 from PySide6.QtWidgets import QApplication
-
-from ..path_model import theme_dir
 
 
 class T:
@@ -20,51 +16,39 @@ class Application(QApplication):
     
     # the holder is made for preventing the objects which were registered to
     # qml side from being recycled by python garbage collector incorrectly.
-    __pyobj_holder: dict[str, QObject]
+    __pyobj_holder: dict[int, QObject]
     
     def __init__(self, app_name='LK QtQuick Scaffold App', **kwargs):
         """
         Args:
             app_name: str
-                Set application name now or later by calling `Application
-                .set_app_name(...)`.
+                set application name.
+                the name can laterly be changed by calling `self.set_app_name`.
             kwargs:
-                organization: str['lib.declare_pyside']
-                    Set an organization name, to avoid error info when we use
+                organization: str[default='dev.likianta.lk_qtquick_scaffold']
+                    set an organization name, this avoids an error from
                     `QtQuick.Dialogs.FileDialog`.
-                    Note: if no organization name set, the error message shows:
+                    note: what did the error look like?
                         QML Settings: The following application identifiers
                         have not been set: QVector("organizationName",
                         "organizationDomain")
-                theme_dir: str[`../theme`]
-                    You can pass a custom dir which includes themes. Otherwise
-                    declare-qml will use its built-in themes (the widely used
-                    and maintained is 'LightClean' theme).
-                    The theme dir structure should be like this:
-                        D:/workspace/xxx
-                        |= custom_theme       # pass this to kwargs['theme_dir']
-                            |= MaterialTheme  # use big camel case
-                                |- MText.qml
-                                |- MRectangle.qml
-                                |- ...
-                                |- qmldir     # don't forget this file
-                            |= OceanTheme
-                            |= BootstrapTheme
-                            |= ...
         """
+        from lk_utils import relpath
+        
         super().__init__()
         
         self.setApplicationName(app_name)
         self.setOrganizationName(kwargs.get(
-            'organization', 'lib.declare_pyside'
+            'organization', 'dev.likianta.lk_qtquick_scaffold'
         ))
         
         self.engine = QQmlApplicationEngine()
         self.root = self.engine.rootContext()
         self.__pyobj_holder = {}
         
-        self._fine_tune()
-        self._register_paths(kwargs.get('theme_dir', theme_dir))
+        self._ui_fine_tune()
+        self.register_qmldir(relpath('../widgets_lib'))
+        self.register_qmldir(relpath('../themes'))
         
         self.on_exit = super().aboutToQuit  # noqa
     
@@ -73,78 +57,60 @@ class Application(QApplication):
         # especially for who imports a global instance `app` from this module.
         self.setApplicationName(name)
     
-    def _fine_tune(self):
-        # set font 'microsoft yahei ui' if platform is windows
+    def _ui_fine_tune(self):
         from platform import system
         if system() == 'Windows':
-            self.setFont('Microsoft YaHei')
+            self.setFont('Microsoft YaHei UI')
     
-    def _register_paths(self, theme_dir: str):
-        # self.add_import_entrance(qmlside_dir)
-        self.add_import_entrance(theme_dir)
-    
-    def add_import_entrance(self, qmldir: str):
+    def register_qmldir(self, qmldir: str):
         """
         Args:
-            qmldir: The absolute path of directory, this dir should include at
-                least one component library subfolder (the subfolder's first
-                letter shoule be capitalized), the subfolder should include a
-                file named 'qmldir' (no suffix with it).
-                See examples:
-                    ../qmlside/qlogger (includes 'LKQmlSide')
-                    ../../theme (includes 'LightClean')
+            qmldir: str dirpath
+                this directory should include at least one sub folder, which is
+                available for qml to import.
+                the sub folders should contain one 'qmldir' file, and multiple
+                '*.qml' files. see example of '../widgets_lib'.
         """
         self.engine.addImportPath(qmldir)
     
-    def register_pyobj(self, obj: QObject, name=''):
+    def register_pyobj(self, instance: QObject, name=''):
         """
-        Register Python object to QML root context. Then we can use it as a
-        global registered property across all QML files.
+        register a QObject based instance to qml global namespace (i.e. qml's
+        root context).
         
-        Notes:
-            This object must inherit from `PySide6.QtCore.QObject`.
-            This mehod should be used before `self.start` running.
-            `declare-qml` will register 'PySide' as a built-in object, please
-            do not use 'PySide' for your custom object, or it may erase all
-            built-in features.
-            
         Args:
-            obj:
-            name: Object name. Suggest using capital camel case, and prefixed
-                with 'Py', for example, 'PySide', 'PyHandler', 'PyHook', etc.
-                If name not defined, we'll use object's class name instead.
+            instance:
+            name:
+                what it named in qml.
+                if not given, will use object's class name.
+                suggest to be pascal case, and prefix with 'Py'. for example,
+                'PyHandler', 'PyImageProvider', 'PyDataTrainer', etc.
+        
+        Warning:
+            use this method before calling `self.start`.
         """
-        name = name or obj.__class__.__name__
-        self.root.setContextProperty(name, obj)
-        self.__pyobj_holder[name] = obj
+        name = name or instance.__class__.__name__
+        self.root.setContextProperty(name, instance)
+        self.__pyobj_holder[id(instance)] = instance
     
     def start(self, qmlfile: T.Path):
         """
         Args:
-            qmlfile: Pass a '.qml' file to launch the application.
-                Usually the filename is 'Main.qml' or 'Homepage.qml', the name
-                case is not sensitive (you can also use lower case if you like).
-                Make sure the file content accords with QML syntax and the root
-                item should be Window.
-        
-        Notice:
-            It seems that `self.engine` cannot recognize abspath format, when
-            we use:
-                self.engine.load(<an_abspath_of_qmlfile>)
-            It will raise a network error:
-                QQmlApplicationengine failed to load component.
-            We should add a prefix 'file:///' to it to resolve this problem.
-            By the way a relative path is always safe to use.
+            qmlfile: str filepath.
+                a '.qml' file. usually it named '~/Main.qml', '~/Home.qml', etc.
+                the name case is not sensitive (you can also use lower case).
         """
-        if xpath.isabs(qmlfile): qmlfile = 'file:///' + qmlfile
-        self.engine.load(qmlfile)
+        from PySide6.QtCore import QUrl
+        # if xpath.isabs(qmlfile): qmlfile = 'file:///' + qmlfile
+        self.engine.load(QUrl(qmlfile))
         self.exec()
-        #   note: do not use `sys.exit(self.exec())` here, that will cause
-        #   `~.py_side.pycomm.PySide` be released in advance. then qml will
-        #   alert 'cannot call from null' in its destroying stage.
+        #   warning: do not use `sys.exit(self.exec())`, because
+        #   `self.__pyobj_holder : values` will be released before qml
+        #   triggered `Component.onDestroyed`. then there will be an error
+        #   'cannot call from null!'
     
-    # function alias for compatibility.
-    launch = run = open = exec_ = start
+    # alias for compatible.
+    launch = run = open = start
     #   https://ux.stackexchange.com/questions/106001/do-we-open-or-launch-or
     #   -startapps+&cd=1&hl=zh-CN&ct=clnk&gl=sg
 
